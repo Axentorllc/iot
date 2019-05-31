f536f1784b4f7702169d60c293e46f6004c1979a
ujjwal kumar <pathakujjwal93@gmail.com>
2019-05-31 18:23:51 +0530

    first commit

---
+++ iot/iot/doctype/compressor_monitor/compressor_monitor.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2019, Vedarth solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('compressor_monitor', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ iot/iot/doctype/compressor_monitor/compressor_monitor.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019, Vedarth solutions and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.model.document import Document
+
+class compressor_monitor(Document):
+	pass
+###demo code provided by Steve Cope at www.steves-internet-guide.com
+##email steve@steves-internet-guide.com
+###Free to use for any purpose
